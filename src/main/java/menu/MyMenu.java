5830e5dbaac1d8b7bbc7f62fe051e94db05f4162
kk2170 <chobi.kimura@gmail.com>
2019-03-25 16:12:34 +0900

    不要なコードの除去

--- src/main/java/menu/MyMenu.java
+++ src/main/java/menu/MyMenu.java
@@ -8,12 +8,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,25 +28,16 @@ public interface MyMenu {
     default List<Item> readJson(String pathName) {
         List<Item> menu = null;
         try {
-            URI uri = ClassLoader.getSystemResource(pathName).toURI();
-
             String jsonString;
-            if (uri.toString().contains("!")) {
-                InputStream in = getClass().getResourceAsStream("/" + pathName);
-                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                jsonString = reader.lines().collect(Collectors.joining());
-            } else {
-                Path path = Paths.get(uri);
-                jsonString = Files.lines(path, StandardCharsets.UTF_8).collect(Collectors.joining(System.getProperty("line.separator")));
-            }
+            InputStream in = getClass().getResourceAsStream("/" + pathName);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            jsonString = reader.lines().collect(Collectors.joining());
 
             ObjectMapper mapper = new ObjectMapper();
             menu = mapper.readValue(jsonString, new TypeReference<List<Item>>() {
             });
             addFee(menu);
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
             e.printStackTrace();
         }
         return menu;
