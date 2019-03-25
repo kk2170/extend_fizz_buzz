39445129a9da338676488b3312c532260663d6ea
kk2170 <chobi.kimura@gmail.com>
2019-03-25 16:31:01 +0900

    ScriptEngineを利用してJacksonを除去

--- src/main/java/menu/MyMenu.java
+++ src/main/java/menu/MyMenu.java
@@ -1,14 +1,17 @@
 package menu;
 
 import JSON.Item;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public interface MyMenu {
@@ -27,19 +30,38 @@ public interface MyMenu {
 
     default List<Item> readJson(String pathName) {
         List<Item> menu = null;
-        try {
-            String jsonString;
-            InputStream in = getClass().getResourceAsStream("/" + pathName);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-            jsonString = reader.lines().collect(Collectors.joining());
 
-            ObjectMapper mapper = new ObjectMapper();
-            menu = mapper.readValue(jsonString, new TypeReference<List<Item>>() {
-            });
-            addFee(menu);
-        } catch (IOException e) {
+        String jsonString;
+        InputStream in = getClass().getResourceAsStream("/" + pathName);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        jsonString = reader.lines().collect(Collectors.joining());
+
+        menu = translateJsonToList(jsonString);
+
+        addFee(menu);
+
+        return menu;
+    }
+
+    default List<Item> translateJsonToList(String jsonString) {
+        List<Item> itemList = new ArrayList<>();
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("JavaScript");
+        try {
+            ScriptObjectMirror json = (ScriptObjectMirror) engine.eval("JSON");
+            Map<String, Map<String, Object>> result = ((Map<String, Map<String, Object>>) json.callMember("parse", jsonString));
+
+            for(String key:result.keySet()){
+                Map<String, Object> map = result.get(key);
+                Item item = new Item();
+                item.category = String.valueOf(map.get("category"));
+                item.price = (Integer) map.get("price");
+                item.name = String.valueOf(map.get("name"));
+                itemList.add(item);
+            }
+        } catch (ScriptException e) {
             e.printStackTrace();
         }
-        return menu;
+        return itemList;
     }
 }
